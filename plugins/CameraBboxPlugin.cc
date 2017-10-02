c9c054ee9b13f362c51b21fcefc7835079d93487
rohit <rohitsalem@gmail.com>
2017-10-02 11:28:43 -0700

    added plugins for getting 3d to 2d pixels

---
+++ plugins/CameraBboxPlugin.cc
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifdef _WIN32
+  // Ensure that Winsock2.h is included before Windows.h, which can get
+  // pulled in by anybody (e.g., Boost).
+#include <Winsock2.h>
+#endif
+
+#include "gazebo/sensors/DepthCameraSensor.hh"
+#include "CameraBboxPlugin.hh"
+#include <ignition/math.hh>
+
+using namespace gazebo;
+GZ_REGISTER_SENSOR_PLUGIN(CameraBboxPlugin);
+
+/////////////////////////////////////////////////
+CameraBboxPlugin::CameraBboxPlugin()
+: SensorPlugin(), width(0), height(0), depth(0)
+{
+}
+
+// void CameraBboxPlugin::Init()
+// {
+//   ignition::math::Vector3d pttest;
+//   pttest.Set(4.5,0,0);
+//   this->width = this->camera->ImageWidth();
+//   this->height = this->camera->ImageHeight();
+//   this->depth = this->camera->ImageDepth();
+//   this->format = this->camera->ImageFormat();
+//   auto pixeltest = this->camera->Project(pttest);
+//   std::cout << pixeltest << '\n' << this->camera->WorldPose() << std::endl;
+//
+// }
+/////////////////////////////////////////////////
+CameraBboxPlugin::~CameraBboxPlugin()
+{
+  this->parentSensor.reset();
+  this->camera.reset();
+}
+
+/////////////////////////////////////////////////
+void CameraBboxPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/)
+{
+  if (!_sensor)
+    gzerr << "Invalid sensor pointer.\n";
+
+  this->parentSensor =
+    std::dynamic_pointer_cast<sensors::CameraSensor>(_sensor);
+
+  if (!this->parentSensor)
+  {
+    gzerr << "CameraBboxPlugin requires a CameraSensor.\n";
+    if (std::dynamic_pointer_cast<sensors::DepthCameraSensor>(_sensor))
+      gzmsg << "It is a depth camera sensor\n";
+  }
+
+  this->camera = this->parentSensor->Camera();
+
+  if (!this->parentSensor)
+  {
+    gzerr << "CameraBboxPlugin not attached to a camera sensor\n";
+    return;
+  }
+  ignition::math::Vector3d ptA, ptB, ptC, ptD, ptE, ptF, ptG , ptH,ptI;
+  // ptA.Set(4.775,1.6,0.2);
+  // ptB.Set(4.225,1.6,0.2);
+  // ptC.Set(4.225,0.8,0.2);
+  // ptD.Set(4.775,0.8,0.2);
+  // ptE.Set(4.775,1.6,2.1);
+  // ptF.Set(4.225,1.6,2.1);
+  // ptG.Set(4.225,0.8,2.1);
+  // ptH.Set(4.775,0.8,2.1);
+  // ptA.Set(4.775,0.4,0.2);
+  // ptB.Set(4.225,0.4,0.2);
+  // ptC.Set(4.225,-0.4,0.2);
+  // ptD.Set(4.775,-0.4,0.2);
+  // ptE.Set(4.775,0.4,2.1);
+  // ptF.Set(4.225,0.4,2.1);
+  // ptG.Set(4.225,-0.4,2.1);
+  // ptH.Set(4.775,-0.4,2.1);
+  ptI.Set(4.6,0,0);
+  this->width = this->camera->ImageWidth();
+  this->height = this->camera->ImageHeight();
+  this->depth = this->camera->ImageDepth();
+  this->format = this->camera->ImageFormat();
+  this->dataPtr->connections.push_back(event::Events::ConnectPreRender(std::bind(&CameraBboxPlugin::pixel, this)));
+
+  // auto pixelsA = this->camera->Project(ptA);
+  // auto pixelsB = this->camera->Project(ptB);
+  // auto pixelsC = this->camera->Project(ptC);
+  // auto pixelsD = this->camera->Project(ptD);
+  // auto pixelsE = this->camera->Project(ptE);
+  // auto pixelsF = this->camera->Project(ptF);
+  // auto pixelsG = this->camera->Project(ptG);
+  // auto pixelsH = this->camera->Project(ptH);
+  auto pixelsI = this->camera->Project(ptI);
+  // std::cout << "pixels" << pixelsA << '\n' << pixelsB << "\n" << pixelsC << "\n" << pixelsD << "\n";
+  // std::cout << "pixels" << pixelsE << '\n' << pixelsF << "\n" << pixelsG << "\n" << pixelsH << "\n";
+std::cout << pixelsI << '\n' << this->camera->WorldPose() << std::endl;
+  this->newFrameConnection = this->camera->ConnectNewImageFrame(
+      std::bind(&CameraBboxPlugin::OnNewFrame, this,
+        std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,
+        std::placeholders::_4, std::placeholders::_5));
+
+  this->parentSensor->SetActive(true);
+}
+
+void CameraBboxPlugin::pixel()
+{
+  ignition::math::Vector3d pttest;
+  pttest.Set(4.5,0,0);
+  this->width = this->camera->ImageWidth();
+  this->height = this->camera->ImageHeight();
+  this->depth = this->camera->ImageDepth();
+  this->format = this->camera->ImageFormat();
+  auto pixeltest = this->camera->Project(pttest);
+  std::cout << pixeltest << '\n' << this->camera->WorldPose() << std::endl;
+
+}
+/////////////////////////////////////////////////
+void CameraBboxPlugin::OnNewFrame(const unsigned char * /*_image*/,
+                              unsigned int /*_width*/,
+                              unsigned int /*_height*/,
+                              unsigned int /*_depth*/,
+                              const std::string &/*_format*/)
+{
+  /*rendering::Camera::SaveFrame(_image, this->width,
+    this->height, this->depth, this->format,
+    "/tmp/camera/me.jpg");
+    */
+}
