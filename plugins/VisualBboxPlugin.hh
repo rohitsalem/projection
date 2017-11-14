66d9186fde1c9d83ded22175627cee6d30e44b43
rohitsalem <rohitsalem@gmail.com>
2017-11-14 15:45:58 -0800

    Moved matrix computation to the VisualBBox plugin

--- plugins/VisualBboxPlugin.hh
+++ plugins/VisualBboxPlugin.hh
@@ -5,6 +5,7 @@
 #include <memory>
 #include <ros/ros.h>
 #include <std_msgs/Float64MultiArray.h>
+#include "geometry.h"
 
 namespace gazebo
 {
@@ -27,14 +28,19 @@ namespace gazebo
 
 	/// Defining variables to store the x,y,z coordiantes of min and max corners
 	double x_min, y_min, z_min, x_max, y_max, z_max;
-	/// Array to store the corners:
-	std_msgs::Float64MultiArray corners;
+
 	/// \brief	Callback to receive info.
 	private: void OnInfo(ConstPosesStampedPtr &_msg);
 
 	/// \internal
 	/// \brief Private Data pointer
 	private: std::unique_ptr<VisualBboxPluginPrivate> dataPtr;
+
+	private: Vec3f pWorldA, pWorldB, pWorldC, pWorldD, pWorldE, pWorldF, pWorldG, pWorldH;
+	private: float x_world, y_world, z_world, roll_world, pitch_world, yaw_world ;
+	private: std_msgs::Float64MultiArray worldArr;
+	private: Matrix44f RT;
+	private: void computeTransformationMatrix(float r , float p, float y, Vec3f c);
 
 	};
 }
